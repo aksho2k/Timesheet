165a766fd04e56bce00a9ec973c06917f82244bf
Akshobhya R <akshobhya10@gmail.com>
2026-06-03 17:32:27 +0530

    Initial Commit

---
+++ Timesheet/TimesheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesheetApp.swift
+//  Timesheet
+//
+//  Created by Akshobhya on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TimesheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
